bdd44a338b99ee08be1e2579955aba55f2d6977b
wizardfan@gmail.com <wizardfan@gmail.com@9acfa245-ff44-dad0-731e-66d1af747e1a>
2014-03-11 16:26:51 +0000

    1. rewording in speciesFinding 2. add mgf output type to proteowizard 3. corresponding wiki page updated

--- trunk/wrappers/PSIstandards/proteowizard/proteowizard.pl
+++ trunk/wrappers/PSIstandards/proteowizard/proteowizard.pl
@@ -4,8 +4,9 @@ use strict;
 my $input = $ARGV[0];
 my $type = $ARGV[1];
 my $output = $ARGV[2];
-my $id = $ARGV[3];
-my $path = $ARGV[4];
+my $outputType = $ARGV[3];
+my $id = $ARGV[4];
+my $path = $ARGV[5];
 
 my $os = lc($^O);
 print "OS: $os\n";
@@ -16,16 +17,16 @@ if($os eq "mswin32" || $os eq "cygwin" || $os eq "dos" || $os eq "os2"){
 	
 #	print ("copy $input $tmpFolder\\tmp_$id.$type\n");
 	system("copy $input $tmpFolder\\tmp_$id.$type");
-#	print ("msconvert $tmpFolder\\tmp_$id.$type --outfile tmp_$id.mzML -o $tmpFolder\n");
-	system("msconvert $tmpFolder\\tmp_$id.$type --outfile tmp_$id.mzML -o $tmpFolder");
-#	print ("move $tmpFolder\\tmp_$id.mzML $output\n");
-	system("move $tmpFolder\\tmp_$id.mzML $output");
+#	print ("msconvert $tmpFolder\\tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $tmpFolder\n");
+	system("msconvert $tmpFolder\\tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $tmpFolder");
+#	print ("move $tmpFolder\\tmp_$id.$outputType $output\n");
+	system("move $tmpFolder\\tmp_$id.$outputType $output");
 #	print ("del $tmpFolder\\tmp_$id.$type\n");
 	system("del $tmpFolder\\tmp_$id.$type");
 }else{
 	system("cp $input /$path/tmp_$id.$type");
-	system("/$path/../../../gio_applications/proteowizard/msconvert /$path/tmp_$id.$type --outfile tmp_$id.mzML -o $path");
-	system("mv /$path/tmp_$id.mzML $output");
+	system("/$path/../../../gio_applications/proteowizard/msconvert /$path/tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $path");
+	system("mv /$path/tmp_$id.$outputType $output");
 	system("rm /$path/tmp_$id.$type");
 }
 
