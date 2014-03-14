4cf7fd59a6068d6d6db1077b2005f67fb583d42d
wizardfan@gmail.com <wizardfan@gmail.com@9acfa245-ff44-dad0-731e-66d1af747e1a>
2014-03-14 14:14:29 +0000

    add merge function to msconvert also the name changed from "convert spectra file" to "MSConvert" corresponding wiki updated

--- trunk/wrappers/PSIstandards/proteowizard/proteowizard.pl
+++ trunk/wrappers/PSIstandards/proteowizard/proteowizard.pl
@@ -1,33 +1,58 @@
 #!/usr/bin/perl -w
 use strict;
-
-my $input = $ARGV[0];
-my $type = $ARGV[1];
-my $output = $ARGV[2];
-my $outputType = $ARGV[3];
-my $id = $ARGV[4];
-my $path = $ARGV[5];
+my $inputSize = (scalar @ARGV)-5;
+print "input size $inputSize\n";
+my $type = $ARGV[$inputSize+0];
+my $output = $ARGV[$inputSize+1];
+my $outputType = $ARGV[$inputSize+2];
+my $id = $ARGV[$inputSize+3];
+my $path = $ARGV[$inputSize+4];
 
 my $os = lc($^O);
 print "OS: $os\n";
 #http://perldoc.perl.org/perlport.html#DOS-and-Derivatives
+my $removeCmd;
 if($os eq "mswin32" || $os eq "cygwin" || $os eq "dos" || $os eq "os2"){
-#	$deleteCmd = "del";
 	my $tmpFolder = "C:\\lwr_staging\\tmp";
-	
-#	print ("copy $input $tmpFolder\\tmp_$id.$type\n");
-	system("copy $input $tmpFolder\\tmp_$id.$type");
-#	print ("msconvert $tmpFolder\\tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $tmpFolder\n");
-	system("msconvert $tmpFolder\\tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $tmpFolder");
-#	print ("move $tmpFolder\\tmp_$id.$outputType $output\n");
+	if($inputSize == 1){
+		system("copy $ARGV[0] $tmpFolder\\tmp_$id.$type");
+		system("msconvert $tmpFolder\\tmp_$id.$type --$outputType --outfile tmp_$id.$outputType -o $tmpFolder");
+		$removeCmd = "del $tmpFolder\\tmp_$id.$type";
+	}else{
+		my $convertCmd = "msconvert";
+		$removeCmd = "del";
+		for(my $i=0;$i<$inputSize;$i++){
+			system("copy $ARGV[$i] $tmpFolder\\tmp_${id}_$i.$type");
+			$convertCmd .= " $tmpFolder\\tmp_${id}_$i.$type";
+			$removeCmd .= " $tmpFolder\\tmp_${id}_$i.$type";
+		}
+		$convertCmd .= " --merge --$outputType --outfile tmp_$id.$outputType -o $tmpFolder";
+		system($convertCmd);
+	}
 	system("move $tmpFolder\\tmp_$id.$outputType $output");
-#	print ("del $tmpFolder\\tmp_$id.$type\n");
-	system("del $tmpFolder\\tmp_$id.$type");
-}else{
-	system("cp $input /$path/tmp_$id.$type");
-	system("/$path/../../../gio_applications/proteowizard/msconvert /$path/tmp_$id.$type --$outputType --outfile tmp_$id.mzML -o $path");
+	system($removeCmd);
+}else{#linux
+	if($inputSize == 1){
+		system("cp $ARGV[0] /$path/tmp_$id.$type");
+		system("/$path/../../../gio_applications/proteowizard/msconvert /$path/tmp_$id.$type --$outputType --outfile tmp_$id.$outputType -o $path");
+		$removeCmd = "rm /$path/tmp_$id.$type";
+	}else{
+		my $convertCmd = "/$path/../../../gio_applications/proteowizard/msconvert";
+		$removeCmd = "rm";
+		for(my $i=0;$i<$inputSize;$i++){
+#			print ("cp $ARGV[$i] /$path/tmp_${id}_$i.$type\n");
+			system("cp $ARGV[$i] /$path/tmp_${id}_$i.$type");
+			$convertCmd .= " /$path/tmp_${id}_$i.$type";
+			$removeCmd .= " /$path/tmp_${id}_$i.$type";
+		}
+		$convertCmd .= " --merge --$outputType --outfile tmp_$id.$outputType -o $path";
+#		print "convert command: $convertCmd\n";
+		system($convertCmd);
+	}
 	system("mv /$path/tmp_$id.$outputType $output");
-	system("rm /$path/tmp_$id.$type");
+#	print("mv /$path/tmp_$id.$outputType $output\n");
+#	print "remove command: $removeCmd\n";
+	system($removeCmd);
 }
 
 #print "Input file: $input\n";
