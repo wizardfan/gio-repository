ad678a615f3b9bea0bb4f4471c5ca73c2e89d162
wizardfan@gmail.com <wizardfan@gmail.com@9acfa245-ff44-dad0-731e-66d1af747e1a>
2014-05-02 21:12:51 +0000

    1. add multiple core setting 2. add searchgui 3. update mzidlib post processing CV terms 4. first attempt to separate javaLib from jar file: mzidlib

---
+++ applications/searchgui/searchgui.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl -w
+use strict;
+#define the temp folder for the middle files
+my $tmpFolder="/tmp/searchgui";
+system ("mkdir $tmpFolder") unless (-d $tmpFolder);
+
+my $config = $ARGV[0];#the config file contains all settings
+my $output = $ARGV[1];#the output file, should be temporary
+my $path = $ARGV[2];#galaxy temp folder, used to locate executables
+my $id = $ARGV[3];#new job id to be distinguished from other SearchGUI search
+
+my $version="1.18.2";
+my $applicationPath = "$path/../../../gio_applications";
+my $jarFile = "$applicationPath/searchgui/SearchGUI/SearchGUI-${version}.jar";
+unless (-e $jarFile){
+	print OUT "Error: Cannot locate the jar file at $jarFile\n";
+	exit 1;
+}
+
+open OUT,">$output";
+print OUT "New file folder: $path\n";
+print OUT "New job id: $id\n\n";
+
+my $line;
+open IN,"$config";
+my @spectra;
+$line = <IN>;#the spectra header
+while($line=<IN>){
+	chomp $line;
+	last if (length $line==0);
+	push (@spectra,$line);
+}
+print OUT "spectra:\n@spectra\n\n";
+
+my $type=<IN>;#the type of the database, either built-in or uploaded file
+chomp $type;
+my $database=<IN>;#the fasta database file
+chomp $database;
+print OUT "$type: $database\n";
+#generate the decoy database
+my $original;
+my $decoy;
+if($type eq "upload"){
+	my $series;
+	my $serial;
+	if($database=~/\/(\d+)\/dataset_(\d+)\.dat$/){
+		$series=$1;
+		$serial=$2;
+	}
+	$original = "$tmpFolder/seq_${series}_${serial}.fasta";
+	$decoy = "$tmpFolder/seq_${series}_${serial}_concatenated_target_decoy.fasta";
+}else{
+	$original = $database;
+	if($original=~/\.fasta$/){
+		$decoy = "$`_concatenated_target_decoy.fasta";	
+	}
+}
+print OUT "Original fasta: $original\n";
+print OUT "Decoy: $decoy\n";
+unless (-e $decoy){
+	print OUT ("cp $database $original\n") if ($type eq "upload");
+	system("cp $database $original") if ($type eq "upload");
+	print OUT "java -cp $jarFile eu.isas.searchgui.cmd.FastaCLI -in $original -decoy\n";
+	system("java -cp $jarFile eu.isas.searchgui.cmd.FastaCLI -in $original -decoy");
+#	system("rm $original") if ($type eq "upload");	
+}
+
+#generate parameter file
+$line=<IN>;#empty line
+$line=<IN>;#header line
+$line=<IN>;
+chomp $line;
+my $identification = $line;
+my $cmd = "java -cp $jarFile eu.isas.searchgui.cmd.IdentificationParametersCLI -db $decoy -out $tmpFolder/search_$id ";
+if($identification =~/ \-fixed_mods (.+) \-variable_mods (.+)$/){
+	$cmd .= $`;
+	$cmd .= " -fixed_mods \"$1\"" unless ($1 eq "None");
+	$cmd .= " -variable_mods \"$2\"" unless ($2 eq "None");
+}
+print OUT "$cmd\n";
+system($cmd);
+
+#execute SearchGUI command line
+$line=<IN>;#empty line
+$line=<IN>;#header line
+$line=<IN>;
+chomp $line;
+my $search = $line;
+print OUT "search from config: $search\n";
+my $xtandem;
+my $msgf;
+my $omssa;
+if($search =~ /-xtandem (\d) -msgf (\d) -omssa (\d)/){
+	$xtandem = $1;
+	$msgf = $2;
+	$omssa = $3;
+}
+print OUT ("mkdir $tmpFolder/result$id\n");
+system("mkdir $tmpFolder/result$id");
+my $searchCmd = "java -cp $jarFile eu.isas.searchgui.cmd.SearchCLI -spectrum_files ";
+my @mgf;
+my @historyIDs;
+for (my $i=0;$i<scalar @spectra;$i++){
+	my ($spectra,$hid) = split(" ",$spectra[$i]);
+	my $mgf = "$tmpFolder/spectra_${id}_$i.mgf";
+	system ("cp $spectra $mgf");
+	push (@mgf, $mgf); 
+	push (@historyIDs, $hid);
+}
+my $spectraStr = join(",", @mgf);
+$searchCmd .= "$spectraStr -output_folder $tmpFolder/result$id -id_params $tmpFolder/search_$id.parameters $search 2> /dev/null";
+print OUT "search command: $searchCmd\n\n\n";
+#open CMD, "$searchCmd |";
+#while(<CMD>){
+#	print OUT "$_";
+#}
+system ($searchCmd);
+
+#open CMD, "pwd |";
+#while(<CMD>){
+#	print OUT "current path: $_";
+#}
+
+#deal with the result files
+for (my $i=0;$i < scalar @spectra;$i++){
+	my $prefix = "$tmpFolder/result$id/spectra_${id}_$i";
+	if($msgf == 1){
+		system("cp ${prefix}.mzid $path/primary_${id}_MSGF+ResultOn$historyIDs[$i]_visible_mzid");
+	}
+	if($omssa == 1){
+		system ("java -jar $applicationPath/mzidlib/mzidlib-1.6-javaLib.jar Omssa2mzid ${prefix}.omx ${prefix}-omssa.mzid -outputFragmentation false -decoyRegex REVERSED -omssaModsFile $tmpFolder/result$id/mods.xml -userModsFile $tmpFolder/result$id/usermods.xml -compress false");
+		system("cp ${prefix}-omssa.mzid $path/primary_${id}_OMSSAresultOn$historyIDs[$i]_visible_mzid");
+	}
+	if($xtandem == 1){
+		system ("java -jar $applicationPath/mzidlib/mzidlib-1.6-javaLib.jar Tandem2mzid ${prefix}.t.xml ${prefix}-tandem.mzid -outputFragmentation false -decoyRegex REVERSED -databaseFileFormatID MS:1001348 -massSpecFileFormatID MS:1001062 -idsStartAtZero false -proteinCodeRegex \\S+ -compress false");
+		system("cp ${prefix}-tandem.mzid $path/primary_${id}_XTandemResultOn$historyIDs[$i]_visible_mzid");
+	}
+}
+
+close OUT;
+opendir DIR, "$tmpFolder/result$id/";
+my @files = readdir DIR;
+foreach my $file(@files){
+	if ($file=~/SearchGUI Report .+\.html$/){
+		print "cp \"$tmpFolder/result$id/$file\" $output\n";
+		system ("cp \"$tmpFolder/result$id/$file\" $output");
+		last;
+	}
+}
+
