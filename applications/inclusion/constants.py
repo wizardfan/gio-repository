2ac73e4a83312d3e950ca3d3bd56db8f2b5c9e8c
wizardfan@gmail.com <wizardfan@gmail.com@9acfa245-ff44-dad0-731e-66d1af747e1a>
2015-04-09 14:10:01 +0000

    1. add tool Interference calculation 2. slightly improve the comments in pitGalaxy.pl

---
+++ applications/inclusion/constants.py
@@ -0,0 +1,8 @@
+# Defines constants for intercalc
+
+# file column numbers
+PEPTIDE_COL   = 0
+PROTEIN_COL   = 1
+MOLWEIGHT_COL = 2
+HYDRO_COL     = 3
+INTERFERE_COL = 4
---
+++ applications/inclusion/intercalc.py
@@ -0,0 +1,50 @@
+# LC-MS Interferent Calculator
+# Developed with Python 2.7.8
+
+# public library imports
+import argparse
+
+# local imports
+from background import *
+from constants import *
+
+# define and parse command line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("targetfile", help="input file containing target peptides")
+parser.add_argument("backgroundfile", help="input file containing background peptides")
+parser.add_argument("outputfile", help="output file containing target peptides with interference information")
+parser.add_argument("massaccuracy", help="mass accuracy in Daltons", type=float)
+parser.add_argument("peakwidth", help="width of eluction peaks in hydrophobicity units", type=float)
+parser.add_argument("-v", "--verbose", action="store_true", help="print progress information to console")
+args = parser.parse_args()
+
+if args.verbose:
+  print "\nCreating background object ..."
+
+# create background object by reading interferent file
+b = Background(args.backgroundfile, args.massaccuracy, args.peakwidth, args.verbose)
+
+# open new file (to write results to)
+outfile = open(args.outputfile, "w")  # output always called intercalc.tsv
+
+# open target file (read only)
+infile = open(args.targetfile, "r")
+
+# append interferent probablity column heading
+outfile.write(infile.readline().rstrip()+"\tInteferent score\n")
+if args.verbose:
+  print "Annotating targets with interference data "
+
+# step through target peptides one by one and append calcualted interferent probability
+for line in infile:
+  # extract mass and hydrophobicity from line (could there be a one line way to do this?)
+  splitline = line.split("\t")
+  mass = float(splitline[MOLWEIGHT_COL])
+  h = float(splitline[HYDRO_COL])
+  # ask background object for peptide-specific interfrence information
+  ptotal = b.interference(mass, h, args.verbose)
+  # write peptide line with appended ptotal to new file
+  outfile.write(line.rstrip() + "\t" + str(ptotal) + "\n")
+
+infile.close()
+outfile.close()
