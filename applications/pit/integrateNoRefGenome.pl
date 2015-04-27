74ff24a56aeeadf18d7ed026d8fd05a4f5755652
wizardfan <j.fan@qmul.ac.uk>
2015-04-27 17:40:50 +0100

    minor bug correction for extracting identified protein sequence
    some header may have ",," which will cause an error message though it
    should not affect the results

--- applications/pit/integrateNoRefGenome.pl
+++ applications/pit/integrateNoRefGenome.pl
@@ -13,6 +13,7 @@ my $peptidesFile = $ARGV[1];
 my $outFile = $ARGV[2];
 
 my %seqs = %{&readFasta($fastaFile)};
+
 my %proteins;
 # the header in the format-pre-defined file in the order of
 # 0 peptide, 1 proteins, 2 Reverse, 3 Contamination, 4 Confidence value (could be score, p-value etc), 5 quantitation value, 6+ optional
@@ -25,7 +26,7 @@ while (my $line = <IN>){
 	#next if ($elmts[2] eq '+' || $elmts[3] eq '+');#2 Reverse 3 contaminant, if true that record should not be used
 	my $peptide = $elmts[0];
 	my $protein_name = $elmts[1];#Protein, e.g. >96407_5 [2245 - 587] (REVERSE SENSE);>96409_5 [2116 - 566] (REVERSE SENSE);>96408_5 [2043 - 613] (REVERSE SENSE),96410_5 [2172 - 742] (REVERSE SENSE)
-	next if (length $protein_name == 0);#decoy hit in MS-GF+
+	next if (length $protein_name == 0);#it could happen e.g. decoy hit in MS-GF+. Because no protei id, no protein sequence, so jump to next
 	$protein_name =~ s/,/>/g; #replace , with >
 	my @protein_entries = split(/>/, $protein_name); #some elements may have ; at the end which does not matter due to the RE
 	for (my $i=1;$i<scalar @protein_entries;$i++){#start from index 1 as the first element is always empty due to the fasta header format
@@ -70,12 +71,17 @@ sub readFasta(){
 		chomp $line;
 		if($line=~/^>/){
 			unless(length $seq==0){
+				#the header can not be directly used
+				#In the PIT pipeline, one ORF (protein) can be from multiple transcripts which is separated by ,
 				$header =~ s/,/>/g; #replace , with >
 				my @tmp = split(/>/, $header); 
-				for (my $i=0;$i<scalar @tmp;$i++){#start from index 1 as the first element is always empty due to the fasta header format
+				for (my $i=0;$i<scalar @tmp;$i++){
 					#in the mzIdentML file only the first part may be used as accession
 					my $id = $tmp[$i];
+					next if (length($id)==0);
+					#print "current id: <$id>\n";
 					$seqs{$id}=$seq;
+					#only keep the accession before first space
 					my ($first) = split(" ",$id);
 					$seqs{$first} = $seq;
 				}
@@ -89,8 +95,9 @@ sub readFasta(){
 	unless(length $seq==0){
 		$header =~ s/,/>/g; #replace , with >
 		my @tmp = split(/>/, $header); 
-		for (my $i=0;$i<scalar @tmp;$i++){#start from index 1 as the first element is always empty due to the fasta header format
+		for (my $i=0;$i<scalar @tmp;$i++){
 			my $id = $tmp[$i];
+			next if (length($id)==0);
 			$seqs{$id}=$seq;
 			my ($first) = split(" ",$id);
 			$seqs{$first} = $seq;
